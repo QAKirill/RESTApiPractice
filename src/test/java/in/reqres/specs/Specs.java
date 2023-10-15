168d4d212a2713d15bf14c23790c5622e42302ab
Kirill <ghostman92@ya.ru>
2023-10-15 18:38:35 +0300

    Убрано дублирование в отчетах Аллюр

--- src/test/java/in/reqres/specs/Specs.java
+++ src/test/java/in/reqres/specs/Specs.java
@@ -12,8 +12,7 @@ import static io.restassured.filter.log.LogDetail.STATUS;
 import static io.restassured.http.ContentType.JSON;
 
 public class Specs {
-    public static RequestSpecification requestSpec = with().filter(withCustomTemplates())
-            .filter(new AllureRestAssured())
+    public static RequestSpecification requestSpec = with()
             .filter(withCustomTemplates())
             .log().uri()
             .log().method()
